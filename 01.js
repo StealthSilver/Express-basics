6ac4a8ff777a7346697e21314a566cb061f3cab7
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2024-11-27 12:36:36 +0530

    express commands

--- 01.js
+++ 01.js
@@ -9,3 +9,28 @@ app.listen(port, () => {
 
 // this starts a server , to stop press control + C
 // check the server on localhost:3000
+
+app.use((req, res) => {
+
+    console.log("request recieved");
+
+    //string response 
+
+    // res.send("this is a basic response");
+
+    //object as response
+
+    // res.send({
+    //     name: "apple",
+    //     color: "red"
+    // })
+
+    // html response
+
+    res.send("<h1>Fruits</h1> <ul><li>apple</li> <li> Orange</li></ul>")
+
+})
+
+app.use((req, res) => {
+    console.log("request recieved");
+})
